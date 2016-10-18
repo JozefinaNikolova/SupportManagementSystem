1585d97d8bba4e59877a222c7562fae1cc50528b
JozefinaNikolova <jozi5@abv.bg>
2016-10-18 13:04:40 +0300

    Added data project. #1 - Implemented repository pattern.

---
+++ SupportManagementSystem/SMS.Data/ISMSData.cs
@@ -0,0 +1,12 @@
+﻿namespace SMS.Data
+{
+    using SMS.Data.Repositories;
+    using SMS.Models;
+
+    public interface ISMSData
+    {
+        //IRepository<MyEntity> MyEntities { get; }
+
+        int SaveChanges();
+    }
+}
