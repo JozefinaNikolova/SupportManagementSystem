6117cef966c11352b75d3709d111a8b537e42b68
JozefinaNikolova <jozi5@abv.bg>
2016-10-27 13:31:50 +0300

    Changed route for admin controller

--- SupportManagementSystem/SMS.Web/Areas/Administrator/AdministratorAreaRegistration.cs
+++ SupportManagementSystem/SMS.Web/Areas/Administrator/AdministratorAreaRegistration.cs
@@ -1,7 +1,8 @@
-﻿using System.Web.Mvc;
-
-namespace SMS.Web.Areas.Administrator
+﻿namespace SMS.Web.Areas.Administrator
 {
+    using SMS.Web.Areas.Administrator.Controllers;
+    using System.Web.Mvc;
+
     public class AdministratorAreaRegistration : AreaRegistration 
     {
         public override string AreaName 
@@ -14,6 +15,13 @@ namespace SMS.Web.Areas.Administrator
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+            context.MapRoute(
+                "Administrator_Admin",
+                "Administrator/{action}/{id}",
+                new { controller = "Admin", action = "Index", id = UrlParameter.Optional },
+                new { isMethodInHomeController = new RootRouteConstraint<AdminController>() }
+            );
+
             context.MapRoute(
                 "Administrator_default",
                 "Administrator/{controller}/{action}/{id}",
---
+++ SupportManagementSystem/SMS.Web/Areas/Administrator/RootRouteConstraint.cs
@@ -0,0 +1,16 @@
+﻿namespace SMS.Web.Areas
+{
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class RootRouteConstraint<T> : IRouteConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var rootMethodNames = typeof(T).GetMethods().Select(x => x.Name.ToLower());
+            return rootMethodNames.Contains(values["action"].ToString().ToLower());
+        }
+    }
+}
